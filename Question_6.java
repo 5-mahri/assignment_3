9aaa0817c08e27723f608ae1cdfd12f1bdcba1e4
5-mahri <71532978+5-mahri@users.noreply.github.com>
2020-09-29 20:10:17 +0200

    Add files via upload

---
+++ Question_6.java
@@ -0,0 +1,19 @@
+package assignments3;
+
+public class Question_6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int mgCafein = 500;
+		int lethal = 10000;
+		
+		int killDoses= lethal/mgCafein;
+		
+		System.out.println("Number of miligrams in drink: "+mgCafein);
+		System.out.println("It would take about "+killDoses+" drinks for a lethal overdose");
+		
+
+	}
+
+}
