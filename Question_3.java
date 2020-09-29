111a1fc40b0c29f03a834e37184b8ccaf21b2ef7
5-mahri <71532978+5-mahri@users.noreply.github.com>
2020-09-29 20:09:07 +0200

    Add files via upload

---
+++ Question_3.java
@@ -0,0 +1,16 @@
+package assignments3;
+
+public class Question_3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		float area, width,height;
+		width=4.5f;
+		height=7.9f;
+		area=width*height;
+		
+		System.out.println("The area of the rectangle is "+area);
+
+	}
+
+}
