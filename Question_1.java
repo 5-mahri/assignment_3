a1f30b786e65939afa62306a9bdb6f64f3ddec7d
5-mahri <71532978+5-mahri@users.noreply.github.com>
2020-09-29 20:08:08 +0200

    Add files via upload

---
+++ Question_1.java
@@ -0,0 +1,16 @@
+package assignments3;
+
+public class Question_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	    int gallon=15;
+		double Liters;
+	 Liters=3.7854*gallon;
+	 System.out.println(gallon+" gallon is "+Liters+" liter.");
+	 
+
+	}
+
+}
