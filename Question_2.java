a20dbadd55faf0c027d20c2211d9138678c0a089
5-mahri <71532978+5-mahri@users.noreply.github.com>
2020-09-29 20:08:28 +0200

    Add files via upload

---
+++ Question_2.java
@@ -0,0 +1,14 @@
+package assignments3;
+
+public class Question_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 
+		String s="Mahri";
+		int age=31;
+		System.out.println("\"Hello,"+s+"! "+"Based on your input, your age is "+age+" :)\"");
+
+	}
+
+}
