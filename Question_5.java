46e08e9c2058ef708be2896d3a263788bb121835
5-mahri <71532978+5-mahri@users.noreply.github.com>
2020-09-29 20:09:52 +0200

    Add files via upload

---
+++ Question_5.java
@@ -0,0 +1,19 @@
+package assignments3;
+
+public class Question_5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int hours, minutes, seconds, inputSeconds;
+		hours=1;
+		minutes=1;
+		seconds=35;
+		inputSeconds=(hours*60)*60+(minutes*60)+seconds;
+		
+		System.out.println(hours+" hours, "+minutes+" minutes, and "+seconds+" seconds");
+		System.out.println("inputSeconds is "+inputSeconds);
+
+	}
+
+}
