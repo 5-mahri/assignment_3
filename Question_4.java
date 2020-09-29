bb72bd28e64014e0a9d1dd105dabc68eec82a38a
5-mahri <71532978+5-mahri@users.noreply.github.com>
2020-09-29 20:09:33 +0200

    Add files via upload

---
+++ Question_4.java
@@ -0,0 +1,19 @@
+package assignments3;
+
+public class Question_4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int num1, num2, swap1,swap2;
+		num1=10;
+		num2=20;
+		swap1=num2;
+		swap2=num1;
+        
+		System.out.println("n1="+num1+", "+"n2="+num2);
+		System.out.println("n1="+swap1 +", " + "n2="+swap2);
+
+	}
+
+}
