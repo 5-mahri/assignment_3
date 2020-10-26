df743d6615021ec97bebb6b61d2126375ad5702f
5-mahri <71532978+5-mahri@users.noreply.github.com>
2020-10-26 15:22:16 +0100

    Add files via upload

--- Question_5.java
+++ Question_5.java
@@ -1,19 +1,22 @@
 package assignments3;
 
+import java.util.Scanner;
+
 public class Question_5 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
 		int hours, minutes, seconds, inputSeconds;
-		hours=1;
-		minutes=1;
-		seconds=35;
-		inputSeconds=(hours*60)*60+(minutes*60)+seconds;
+		Scanner object=new Scanner(System.in);
+		System.out.println("Enter seconds:");
+		inputSeconds=object.nextInt();
+		
+		seconds=inputSeconds%60;
+		minutes=inputSeconds/60;
+		minutes=minutes%60;
+		hours=inputSeconds/3600;
+		
 		
 		System.out.println(hours+" hours, "+minutes+" minutes, and "+seconds+" seconds");
-		System.out.println("inputSeconds is "+inputSeconds);
-
 	}
 
 }
